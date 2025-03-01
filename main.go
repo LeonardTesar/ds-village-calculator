7451e7f24b1a429623fe3faa50a3c303172460ba
Leonard Tesar <leonard.tesar@gmx.de>
2025-03-01 01:36:47 +0100

    Arguments are now read and passed onto the other functions

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"ds-village-calculator/TwStatsScraper"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) != 3 {
+		fmt.Println("Please provide the world and village id as arguments")
+		fmt.Println("Scheme: TwStatScraper.exe world villageId")
+		fmt.Println("Example: TwStatScraper.exe de238 4165")
+		return
+	}
+
+	world := arguments[1]
+	villageId, err := strconv.Atoi(arguments[2])
+	if err != nil {
+		fmt.Println("Invalid village id:", err.Error())
+	}
+	buildings := getBuildings(world)
+	villageExpansionTree := NewVillageExpansionTree(buildings...)
+	villageHistory := TwStatsScraper.ScrapeVillageHistory(world, villageId)
+
+	possibleBuildingCompositions := findPossibleBuildingCompositions(villageExpansionTree, villageHistory)
+	for _, village := range possibleBuildingCompositions {
+		for building, level := range village {
+			fmt.Printf("%s: %d", building, level)
+		}
+	}
+}
+
+func findPossibleBuildingCompositions(tree VillageExpansionTree, history []TwStatsScraper.VillageHistoryEntry) []map[string]int {
+	//TODO: implement
+	panic("Not yet implemented")
+}
